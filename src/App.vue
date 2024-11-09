ae28385ada3162dc249c03c59e2b8ffef3a9547f
Yana <yana-kim-2002@mail.ru>
2024-11-09 14:32:59 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { ref, onMounted } from "vue";
+import { RouterLink, RouterView } from "vue-router";
+import Topbar from "./components/Topbar.vue";
+import Sidebar from "./components/Sidebar.vue";
+</script>
+
+<template>
+  <Sidebar />
+  <Topbar />
+  <RouterView />
+</template>
